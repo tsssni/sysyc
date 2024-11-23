0b8a7b9e4c24e08e1b65b3a7300eb48f5d57e1bf
tsssni <dingyongyu2002@foxmail.com>
2024-11-23 15:12:17 +0800

    add dependencies

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "sysyc devenv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    nixpkgs
+    , ...
+  }:
+  let
+    setupShell = shell: system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+      ];
+
+      shellHook = ''
+        exec ${shell}
+      '';
+    };
+  in {
+    devShells."aarch64-darwin".default = setupShell "zsh" "aarch64-darwin";
+    devShells."x86_64-linux".default = setupShell "elvish" "x86_64-linux";
+  };
+}
